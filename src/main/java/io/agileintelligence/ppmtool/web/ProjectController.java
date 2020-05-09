7c4e9c40680da246169db875c2348372046ec52d
Fernando Fresco <ffresco@gmail.com>
2020-05-09 14:22:05 -0300

    part 2 up to class 12

--- src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
+++ src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
@@ -7,9 +7,11 @@ package io.agileintelligence.ppmtool.web;
 
 import io.agileintelligence.ppmtool.domain.Project;
 import io.agileintelligence.ppmtool.services.ProjectService;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,7 +29,12 @@ public class ProjectController {
     private ProjectService projectService;
     
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project , BindingResult result){
+        
+        if (result.hasErrors()) {
+            return new ResponseEntity<String>("Project object has errors",HttpStatus.BAD_REQUEST);
+        }
+        
         Project project1 = projectService.saverOrUpdateProject(project);
         return new ResponseEntity<Project>(project1, HttpStatus.CREATED); 
     }
