16012f635430064183899a415945e2a69bcd6c1f
Fernando Fresco <ffresco@gmail.com>
2020-05-12 12:55:34 -0300

    c 26

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import ProjectItem from "./Project/ProjectItem";
 import Header from "./Layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default class Dashboard extends Component {
   render() {
     return (
       <div>
         <Header />
-        <h1>Welcome to Dashboard</h1>
+        <h1 className="alert alert-warning">Welcome to Dashboard</h1>
         <ProjectItem />
         <ProjectItem />
       </div>
