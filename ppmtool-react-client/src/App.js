9a00b0d6d7fa7af19176c051dc9c41f853c0c911
Fernando Fresco <ffresco@gmail.com>
2020-05-14 10:19:13 -0300

    c 28

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/Layout/Header";
+import AddProject from "./components/Project/AddProject";
 
 function App() {
   return (
-    <div className="App">
-      <Dashboard />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/addProject" component={AddProject} />
+      </div>
+    </Router>
   );
 }
 
