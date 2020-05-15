eb50333c66f56bd109d794859e37f8467e5ffa83
Fernando Fresco <ffresco@gmail.com>
2020-05-15 12:42:22 -0300

    c31 redux b18

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -4,16 +4,20 @@ import Dashboard from "./components/Dashboard";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Layout/Header";
 import AddProject from "./components/Project/AddProject";
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Route exact path="/dashboard" component={Dashboard} />
-        <Route exact path="/addProject" component={AddProject} />
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Header />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/addProject" component={AddProject} />
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
