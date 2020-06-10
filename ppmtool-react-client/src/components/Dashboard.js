aaf772fec133a6df7a401f9e31d7cd484b24bee0
Fernando Fresco <ffresco@gmail.com>
2020-06-10 13:17:38 -0300

    c36 b23

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -13,6 +13,8 @@ class Dashboard extends Component {
   }
 
   render() {
+    const { projects } = this.props.project;
+
     return (
       <div className="projects">
         <div className="container">
@@ -23,8 +25,9 @@ class Dashboard extends Component {
               <CreateProjectButton />
               <br />
               <hr />
-
-              <ProjectItem />
+              {projects.map((project) => (
+                <ProjectItem key={project.id} project={project} />
+              ))}
             </div>
           </div>
         </div>
